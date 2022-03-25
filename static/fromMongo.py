8e0a43f8bfef65e90c1cf0ae589f70cc628fe7c2
Tarun <tarunsai284@gmail.com>
2022-03-25 15:58:00 -0400

    commenting infinite loop to prevent stackoverflow error

--- static/fromMongo.py
+++ static/fromMongo.py
@@ -6,12 +6,12 @@ client = MongoClient("mongodb+srv://<username>:<password>@cluster0.qvnyy.mongodb
 db = client.get_database('Crypto_db')    #this will be the name of our db in mongo
 records = db.Bitcoin_price      #we can have a collection for each type of crypto maybe????
 
-while True:   #infinite loop
-    r = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")     #this will be the connection to CoinGecko
-    if r.status_code == 200:    #status code 200 means everything is 'good to go'
-        data = r.json()     #data is going to be request as json
-        print(data)
-        records.insert_one(data)
-        time.sleep(60)      #will this be set to 24 hours??
-    else:
-        exit()
+# while True:   #infinite loop
+#     r = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")     #this will be the connection to CoinGecko
+#     if r.status_code == 200:    #status code 200 means everything is 'good to go'
+#         data = r.json()     #data is going to be request as json
+#         print(data)
+#         records.insert_one(data)
+#         time.sleep(60)      #will this be set to 24 hours??
+#     else:
+#         exit()
