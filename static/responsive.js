0e07417425315d9fb44ceb907aac906d3f611fbe
Tarun <tarunsai284@gmail.com>
2022-04-10 19:04:33 -0400

    Reseting the button highlight on crypto page click

--- static/responsive.js
+++ static/responsive.js
@@ -3,6 +3,10 @@ const onClick = function() {
     this.classList.add("active");
 }
 
+const homeOnClick = function() {
+    localStorage.removeItem("idActive");
+}
+
 if(localStorage.getItem("idActive")){
     idActive = localStorage.getItem("idActive")
     document.getElementById(idActive).classList.add("active")
@@ -13,3 +17,4 @@ document.getElementById('BNB').onclick = onClick;
 document.getElementById('ETH').onclick = onClick;
 document.getElementById('LTC').onclick = onClick;
 document.getElementById('NEO').onclick = onClick;
+document.getElementById('nav_crypto').onclick = onClick;
